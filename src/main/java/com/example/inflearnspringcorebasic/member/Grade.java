67c1e46c9dc1404dc37f2c5ce0749dcf543ca298
6047198844 <jslee@miridih.com>
2022-02-20 01:55:03 +0900

    feat: add User

---
+++ src/main/java/com/example/inflearnspringcorebasic/member/Grade.java
@@ -0,0 +1,6 @@
+package com.example.inflearnspringcorebasic.member;
+
+public enum Grade {
+    BASIC,
+    VIP
+}
---
+++ src/main/java/com/example/inflearnspringcorebasic/member/MemberService.java
@@ -0,0 +1,7 @@
+package com.example.inflearnspringcorebasic.member;
+
+public interface MemberService {
+    void join(Member member);
+
+    Member findMember(Long memberId);
+}
