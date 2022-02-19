8d04a0715400a37435dfac7eb0ac5d29c452a0a9
6047198844 <jslee@miridih.com>
2022-02-20 01:35:14 +0900

    feat: Initial commit 🎉

---
+++ src/test/java/com/example/inflearnspringcorebasic/InflearnSpringCoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inflearnspringcorebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringCoreBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
