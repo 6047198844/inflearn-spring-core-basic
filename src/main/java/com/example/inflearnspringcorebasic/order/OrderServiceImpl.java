e070af596324381c8982bcb38590ed7cb8319ac8
6047198844 <jslee@miridih.com>
2022-02-20 13:21:43 +0900

    feat: add RateDiscountPolicy

--- src/main/java/com/example/inflearnspringcorebasic/order/OrderServiceImpl.java
+++ src/main/java/com/example/inflearnspringcorebasic/order/OrderServiceImpl.java
@@ -2,13 +2,15 @@ package com.example.inflearnspringcorebasic.order;
 
 import com.example.inflearnspringcorebasic.discount.DiscountPolicy;
 import com.example.inflearnspringcorebasic.discount.FixDiscountPolicy;
+import com.example.inflearnspringcorebasic.discount.RateDiscountPolicy;
 import com.example.inflearnspringcorebasic.member.Member;
 import com.example.inflearnspringcorebasic.member.MemberRepository;
 import com.example.inflearnspringcorebasic.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
